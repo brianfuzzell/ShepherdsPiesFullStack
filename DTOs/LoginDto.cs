5e0152a226f7ac12b581c68141ecc4a73bec2106
Brian Fuzzell <2788883+brianfuzzell@users.noreply.github.com>
2026-07-01 19:09:49 -0500

    Added AuthController, plus login DTOs

---
+++ DTOs/LoginDto.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace ShepherdsPiesControllers.DTOs;
+
+public class LoginDto
+{
+    [Required, EmailAddress]
+    public required string Email { get; set; }
+
+    [Required]
+    public required string Password { get; set; }
+}
